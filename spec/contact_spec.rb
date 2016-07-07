8dbcf37b1f840f7156062ac0f8881ceedfff8eb1
Caleb Stokka and Cameron Poore <caleb+cameron@>
2016-07-07 12:02:20 -0700

    pass first four identifier specs

--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -8,4 +8,27 @@ describe('Contact') do
       expect(test_contact.first_name()).to(eq('Caleb'))
     end
   end
+
+  describe('#last_name') do
+    it 'returns the last name of the contact' do
+      test_contact = Contact.new({:first_name=> 'Caleb', :last_name=> 'Stokka', :job_title=> 'CEO', :company=> 'China Construction Bank', :mailing_address=> 'Beijing', :phone_number=> '86-232-543-3982-00', :email_address=> 'cstokka@chinaconstructionbank.org'})
+      expect(test_contact.last_name()).to(eq('Stokka'))
+    end
+  end
+
+  describe('#job_title') do
+    it 'returns the job title of the contact' do
+      test_contact = Contact.new({:first_name=> 'Caleb', :last_name=> 'Stokka', :job_title=> 'CEO', :company=> 'China Construction Bank', :mailing_address=> 'Beijing', :phone_number=> '86-232-543-3982-00', :email_address=> 'cstokka@chinaconstructionbank.org'})
+      expect(test_contact.job_title()).to(eq('CEO'))
+    end
+  end
+
+  describe('#company') do
+    it 'returns the company of the contact' do
+      test_contact = Contact.new({:first_name=> 'Caleb', :last_name=> 'Stokka', :job_title=> 'CEO', :company=> 'China Construction Bank', :mailing_address=> 'Beijing', :phone_number=> '86-232-543-3982-00', :email_address=> 'cstokka@chinaconstructionbank.org'})
+      expect(test_contact.company()).to(eq('China Construction Bank'))
+    end
+  end
+
+  
 end
