31784340d10d13df4a9ba096275a31164dadc055
Caleb Stokka and Cameron Poore <caleb+cameron@>
2016-07-07 14:48:29 -0700

    add find, save, id, clear, all methods to mailing address

--- lib/mailing_address.rb
+++ lib/mailing_address.rb
@@ -1,6 +1,8 @@
 class MailingAddress
 
-  attr_accessor(:street_address, :city, :state, :zip, :type)
+  @@mailing_addresses = []
+
+  attr_accessor(:street_address, :city, :state, :zip, :type, :id)
 
   define_method(:initialize) do |particulars|
     @street_address = particulars.fetch(:street_address)
@@ -8,5 +10,28 @@ class MailingAddress
     @state = particulars.fetch(:state)
     @zip = particulars.fetch(:zip)
     @type = particulars.fetch(:type)
+    @id = @@mailing_addresses.length().+(1)
+  end
+
+  define_singleton_method(:all) do
+    @@mailing_addresses
+  end
+
+  define_method(:save) do
+    @@mailing_addresses.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@mailing_addresses = []
+  end
+
+  define_singleton_method(:find) do |id|
+    found_mailing_address = nil
+    @@mailing_addresses.each() do |mailing_address|
+      if mailing_address.id().eql?(id)
+        found_mailing_address = mailing_address
+      end
+    end
+    found_mailing_address
   end
 end
--- spec/mailing_address_spec.rb
+++ spec/mailing_address_spec.rb
@@ -2,9 +2,46 @@ require('rspec')
 require('mailing_address')
 
 describe('MailingAddress') do
+  before() do
+    MailingAddress.clear()
+  end
+
   describe('#mailing_address') do
     it 'returns the mailing address of the contact' do
       test_mailing_address = MailingAddress.new({:street_address=> '88 Phillip St', :city=> 'Sydney', :state=> 'Australia', :zip=> 'NSW 2000', :type=> 'corporate'})
+    end
+  end
+
+  describe('#id') do
+    it 'returns the id of the phone number' do
+      test_mailing_address = MailingAddress.new({:street_address=> '88 Phillip St', :city=> 'Sydney', :state=> 'Australia', :zip=> 'NSW 2000', :type=> 'corporate'})
+      test_mailing_address.save()
+      expect(test_mailing_address.id()).to(eq(1))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(MailingAddress.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it 'adds a mailing_address to the array of saved mailing_addresss' do
+      test_mailing_address = MailingAddress.new({:street_address=> '88 Phillip St', :city=> 'Sydney', :state=> 'Australia', :zip=> 'NSW 2000', :type=> 'corporate'})
+      test_mailing_address.save()
+      expect(MailingAddress.all()).to(eq([test_mailing_address]))
+    end
+  end
+
+
+  describe('.find') do
+    it 'returns a mailing_address by its id number' do
+      test_mailing_address = MailingAddress.new({:street_address=> '88 Phillip St', :city=> 'Sydney', :state=> 'Australia', :zip=> 'NSW 2000', :type=> 'corporate'})
+      test_mailing_address.save()
+      test_mailing_address2 = MailingAddress.new({:street_address=> '89 Phillip St', :city=> 'Sydney', :state=> 'Australia', :zip=> 'NSW 2000', :type=> 'corporate'})
+      test_mailing_address2.save()
+      expect(MailingAddress.find(test_mailing_address.id())).to(eq(test_mailing_address))
     end
   end
 end
